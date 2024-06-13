1ae418f6d9a72538d52fe2ebb394f4b1ea4c8e1a
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-06-13 20:39:26 +0700

    try change adam to sgd

--- SOURCE/orl_train.sh
+++ SOURCE/orl_train.sh
@@ -9,6 +9,6 @@ for i in $(seq 0 "$((${shards}-1))"); do
     for j in {0..15}; do
         echo "shard: $((${i}+1))/${shards}, requests: $((${j}+1))/16"
         r=$((${j}*${shards}/5))
-        python sisa.py --model orl_model --train --slices 1 --dataset face_data/orl/orl_info --label "${r}" --epochs 20 --batch_size 16 --learning_rate 0.001 --optimizer adam --chkpt_interval 1 --container "${shards}" --shard "${i}"
+        python sisa.py --model orl_model --train --slices 1 --dataset face_data/orl/orl_info --label "${r}" --epochs 20 --batch_size 16 --learning_rate 0.001 --optimizer sgd --chkpt_interval 1 --container "${shards}" --shard "${i}"
     done
 done
