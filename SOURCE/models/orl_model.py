6fc3c9cdeedacb34ac3816252ff39456931c1b42
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-06-27 11:29:45 +0700

    Models folder correction

--- SOURCE/models/orl_model.py
+++ SOURCE/models/orl_model.py
@@ -5,9 +5,7 @@ import torch.nn as nn
 class orlModel(nn.Module):
     def __init__(self, input_shape, num_classes, *args, **kwargs):
         super(orlModel, self).__init__()
-        orl_vgg_16 = models.vgg16(pretrained=True)
-        orl_vgg_16.classifier[6] = nn.Linear(4096, num_classes)
-        self.model = orl_vgg_16
+        self.model = models.vgg16(num_classes=40)
     def forward(self, x):
         out = self.model(x)
         return out
