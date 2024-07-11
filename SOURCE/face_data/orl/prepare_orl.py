6f51e5b23184b685a8cac7ab4c3ac66f436be247
buidatie <87751329+buidatie@users.noreply.github.com>
2024-07-12 02:45:58 +0700

    Update prepare_orl.py

--- SOURCE/face_data/orl/prepare_orl.py
+++ SOURCE/face_data/orl/prepare_orl.py
@@ -27,7 +27,7 @@ X_train, X_test, y_train, y_test = train_test_split(images, labels, test_size=0.
 train_trans = transforms.Compose([
     transforms.ToPILImage(),
     transforms.Resize(size=(224,224)),
-    transforms.RamdomRotation((0,15)),
+    transforms.RandomRotation((0,15)),
     transforms.RandomAffine(degrees=0, translate=(5/92,5/112)),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
