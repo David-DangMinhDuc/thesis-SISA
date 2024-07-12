9e6f42dce4d4819d91c32af15a6ee328429510dd
buidatie <87751329+buidatie@users.noreply.github.com>
2024-07-12 16:46:35 +0700

    Update orl_predict.sh

--- SOURCE/orl_predict.sh
+++ SOURCE/orl_predict.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 shards=$1
 
 for i in $(seq 0 "$((${shards}-1))"); do
-    for j in {0..15}; do
+    for j in {0..3}; do
         echo "shard: $((${i}+1))/${shards}, requests: $((${j}+1))/4"
         r=$((${j}*${shards}))
         python sisa.py --model orl_model --test --dataset face_data/orl/orl_info --label "${r}" --batch_size 4 --container "${shards}" --shard "${i}"
