19f272d795aa6555f0470666d9d1154af9be54b4
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-07-18 13:50:32 +0700

    AlexNet model class correction

--- SOURCE/models/ar_model.py
+++ SOURCE/models/ar_model.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 
 class arModel(nn.Module):
     def __init__(self, input_shape, num_classes, *args, **kwargs):
-        super(orlModel, self).__init__()
+        super(arModel, self).__init__()
         self.model = models.alexnet(num_classes=num_classes)
     def forward(self, x):
         out = self.model(x)
