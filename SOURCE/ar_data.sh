699f997e4ba5660af8e1d02e42cae537c96fdea8
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-07-18 18:08:08 +0700

    bash files' correction

--- SOURCE/ar_data.sh
+++ SOURCE/ar_data.sh
@@ -19,7 +19,7 @@ fi
 
 for j in {0..15}; do
     r=$((${j}*${shards}/5))
-    acc=$(python aggregation.py --strategy uniform --container "${shards}" --shards "${shards}" --dataset face_data/ar/ar_info --label "${r}")
+    acc=$(python aggregation.py --strategy uniform --container "${shards}" --shards "${shards}" --dataset face_data/orl/orl_info --label "${r}")
     cat containers/"${shards}"/times/shard-*:"${r}".time > "containers/${shards}/times/times.tmp"
     time=$(python time.py --container "${shards}" | awk -F ',' '{print $1}')
     cat containers/"${shards}"/shard-*:"${r}"_"${j}".txt > "containers/${shards}/numOfRetrainPoints.tmp"
--- SOURCE/ar_data_baseline.sh
+++ SOURCE/ar_data_baseline.sh
@@ -20,7 +20,7 @@ fi
 for j in {0..15}; do
     r=$((${j}*${shards}/5))
     shard_idx=0
-    acc=$(python aggregation.py --strategy uniform --container "${shards}" --shards "${shards}" --dataset face_data/ar/ar_info --baseline "${shard_idx}" --label "${r}")
+    acc=$(python aggregation.py --strategy uniform --container "${shards}" --shards "${shards}" --dataset face_data/orl/orl_info --baseline "${shard_idx}" --label "${r}")
     cat containers/"${shards}"/times/shard-*:"${r}".time > "containers/${shards}/times/times.tmp"
     time=$(python time.py --container "${shards}" | awk -F ',' '{print $1}')
     cat containers/"${shards}"/shard-*:"${r}"_"${j}".txt > "containers/${shards}/numOfRetrainPoints.tmp"
