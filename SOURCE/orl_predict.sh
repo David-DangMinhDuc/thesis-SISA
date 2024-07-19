ba4dfc3cd76e53b695d07c197e4263b8c30c4e74
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-07-19 10:17:08 +0700

    orl bash correction (part 3)

--- SOURCE/orl_predict.sh
+++ SOURCE/orl_predict.sh
@@ -8,7 +8,7 @@ shards=$1
 for i in $(seq 0 "$((${shards}-1))"); do
     for j in {0..3}; do
         echo "shard: $((${i}+1))/${shards}, requests: $((${j}+1))/4"
-        r=1#$((${j}*${shards}))
+        r=$((${j}*${shards}))
         python sisa.py --model orl_model --test --dataset face_data/orl/orl_info --label "${j}" --batch_size 4 --container "${shards}" --shard "${i}"
     done
 done
