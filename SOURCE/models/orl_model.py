9075d81b719b9b6f4ff7f9609d0b45045d7d18e8
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-07-21 15:21:55 +0700

    Try vgg16 model - not change classes directly

--- SOURCE/models/orl_model.py
+++ SOURCE/models/orl_model.py
@@ -5,7 +5,10 @@ import torch.nn as nn
 class orlModel(nn.Module):
     def __init__(self, input_shape, num_classes, *args, **kwargs):
         super(orlModel, self).__init__()
-        self.model = models.vgg16(num_classes=num_classes)
+        vgg_16 = models.vgg16(pretrained=True)
+        num_ftrs = vgg_16.classifier[6].in_features
+        vgg_16.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        self.model = vgg_16#models.vgg16(num_classes=num_classes)
     def forward(self, x):
         out = self.model(x)
         return out
