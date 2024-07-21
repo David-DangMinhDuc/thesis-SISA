a84f94dff5235bbf71f94ec0b46dac26eeae51c5
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-07-21 14:21:12 +0700

    Try old orbit in number of requests

--- SOURCE/ar_init.sh
+++ SOURCE/ar_init.sh
@@ -16,6 +16,6 @@ fi
 python distribution.py --shards "${shards}" --distribution uniform --container "${shards}" --dataset face_data/ar/ar_info --label 0
 
 for j in {1..15}; do
-    r=$((${j}*${shards}/16))
+    r=$((${j}*${shards}/5))
     python distribution.py --requests "${r}" --distribution uniform --container "${shards}" --dataset face_data/ar/ar_info --label "${r}"
 done
