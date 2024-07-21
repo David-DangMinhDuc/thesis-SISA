90fa18f73536f2e9aefe0d8d5b2d330ed05af2aa
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-07-21 13:50:31 +0700

    Try alexnet model - not change classes directly

--- SOURCE/models/ar_model.py
+++ SOURCE/models/ar_model.py
@@ -5,7 +5,10 @@ import torch.nn as nn
 class arModel(nn.Module):
     def __init__(self, input_shape, num_classes, *args, **kwargs):
         super(arModel, self).__init__()
-        self.model = models.alexnet(num_classes=num_classes)
+        aln = models.alexnet(pretrained=True)
+        num_ftrs = aln.classifier[6].in_features
+        aln.classifier[6] = nn.Linear(num_ftrs, num_classes)
+        self.model = aln#models.alexnet(num_classes=num_classes)
     def forward(self, x):
         out = self.model(x)
         return out
