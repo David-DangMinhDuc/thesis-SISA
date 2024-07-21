00c32f4a13574c31e54678dc6ca4fd04bc451682
David-DangMinhDuc <minhducdang020102@gmail.com>
2024-07-21 13:12:10 +0700

    Try ar dataloader with 26 classes

--- SOURCE/face_data/ar/dataloader.py
+++ SOURCE/face_data/ar/dataloader.py
@@ -2,7 +2,7 @@ import numpy as np
 import os
 
 pwd = os.path.dirname(os.path.realpath(__file__))
-data = np.load(os.path.join(pwd, 'ar100_data.npy'), allow_pickle=True).reshape((1,))[0]
+data = np.load(os.path.join(pwd, 'ar26_data.npy'), allow_pickle=True).reshape((1,))[0]
 X_train, X_test, y_train, y_test = data['X_train'], data['X_test'], data['y_train'], data['y_test']
 
 def load(indices, method='train'):
